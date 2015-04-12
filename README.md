d5dc7d23a0bea3aa50e848bb0935788047c06d4a
ayva <olga.ayva@gmail.com>
2015-04-12 07:28:15 +0000

    =Spoof

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Olga & Donald assignment
+Olga & Donald assignment2015-04-12T07:28:15Z
---
+++ script.sh
@@ -0,0 +1,15 @@
+echo 2015-04-12T07:28:15Z>> README.md
+git add .
+git commit --date="2015-04-12T07:28:15Z" -m="Spoof"
+echo 2015-04-12T07:08:21Z>> README.md
+git add .
+git commit --date="2015-04-12T07:08:21Z" -m="Spoof"
+echo 2015-04-12T07:06:23Z>> README.md
+git add .
+git commit --date="2015-04-12T07:06:23Z" -m="Spoof"
+echo 2015-04-12T07:05:42Z>> README.md
+git add .
+git commit --date="2015-04-12T07:05:42Z" -m="Spoof"
+echo 2015-04-12T06:56:20Z>> README.md
+git add .
+git commit --date="2015-04-12T06:56:20Z" -m="Spoof"
